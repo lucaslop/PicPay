ecb49739d260ceff58270379cc204604d30c8fa3
Lucas <lucaslopes05597@gmail.com>
2020-07-25 04:00:51 -0300

    Header ajuste

--- src/pages/Config/index.js
+++ src/pages/Config/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import {View} from 'react-native';
-import Icon from 'react-native-vector-icons/AntDesign';
-// import { Container } from './styles';
+import React from "react";
+import { View } from "react-native";
+import Icon from "react-native-vector-icons/AntDesign";
+import { Container, Header, Title, Text, ScrollView } from "./styles";
 
 const Config = () => {
-  return <View />;
+  return (
+    <Container>
+      <Header>
+        <Title>Meu Saldo</Title>
+        <Text>Ajuda</Text>
+      </Header>
+      <ScrollView></ScrollView>
+    </Container>
+  );
 };
 
 Config.navigationOptions = {
-  tabBarLabel: 'Ajustes',
-  tabBarIcon: ({tintColor}) => (
+  tabBarLabel: "Ajustes",
+  tabBarIcon: ({ tintColor }) => (
     <Icon name="setting" size={20} color={tintColor} />
-  ),
+  )
 };
 
 export default Config;
---
+++ src/pages/Config/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components/native";
+export const Container = styled.SafeAreaView`
+  background: #eee;
+  flex: 1;
+`;
+export const ScrollView = styled.ScrollView``;
+export const Header = styled.View`
+  background: #eee;
+  height: 50px;
+  padding: 0 16px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Title = styled.Text`
+  margin-left: 40%;
+  color: #7c7c7c;
+  font-weight: bold;
+  font-size: 17px;
+`;
+
+export const Text = styled.Text`
+  color: #38cd91;
+  font-size: 16px;
+`;
