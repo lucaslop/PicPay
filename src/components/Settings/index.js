5bfbc281cf85bd9348b66caef28891ce9752608c
Lucas <lucaslopes05597@gmail.com>
2020-07-26 01:41:45 -0300

    Splash Screen

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,15 +1,66 @@
 import React from "react";
 import { View } from "react-native";
-import { Container, Image, UserName, Name, Link } from "./styles";
+import {
+  Container,
+  Image,
+  UserName,
+  Name,
+  Link,
+  MenuTitle,
+  Menu,
+  Item,
+  Title,
+  Label
+} from "./styles";
 import avatar from "../../../assets/avatar.png";
 const Settings = () => {
   return (
-    <Container>
-      <Image source={avatar} />
-      <UserName>@LucasLop</UserName>
-      <Name>Lucas Lopes</Name>
-      <Link>Ver meu perfil ></Link>
-    </Container>
+    <>
+      <Container>
+        <Image source={avatar} />
+        <UserName>@LucasLop</UserName>
+        <Name>Lucas Lopes</Name>
+        <Link>Ver meu perfil ></Link>
+      </Container>
+
+      <MenuTitle>Minha Conta</MenuTitle>
+
+      <Menu>
+        <Item>
+          <Title>Meu PicPay</Title>
+          <Label> @lucaslop ></Label>
+        </Item>
+        <Item>
+          <Title>Meu Número</Title>
+          <Label>(61) 000-00000 ></Label>
+        </Item>
+        <Item>
+          <Title>Meu Email</Title>
+          <Label>lucas@gmail.com ></Label>
+        </Item>
+        <Item>
+          <Title>Conta Bancária</Title>
+          <Label>></Label>
+        </Item>
+        <Item>
+          <Title>Taxas e Limites</Title>
+          <Label>></Label>
+        </Item>
+
+        <Item>
+          <Title>Meus Endereços</Title>
+          <Label>></Label>
+        </Item>
+        <Item>
+          <Title>Meus Favoritos</Title>
+          <Label>></Label>
+        </Item>
+        <Item>
+          <Title>Validar Identidade</Title>
+          <Label>Verificado ></Label>
+        </Item>
+      </Menu>
+    </>
   );
 };
 
--- src/components/Settings/styles.js
+++ src/components/Settings/styles.js
@@ -9,18 +9,48 @@ export const Image = styled.Image`
   height: 100px;
   align-self: center;
 `;
+
 export const UserName = styled.Text`
   font-weight: bold;
   font-size: 22px;
   margin-top: 10px;
 `;
+
 export const Name = styled.Text`
   color: grey;
   align-self: center;
   margin-top: 5px;
 `;
+
 export const Link = styled.Text`
   align-self: center;
   margin-top: 5px;
   color: #38cd91;
+`;
+
+export const MenuTitle = styled.Text`
+  color: grey;
+  font-size: 15px;
+  margin-left: 10px;
+  text-transform: uppercase;
+`;
+export const Menu = styled.View`
+  flex: 1;
+  margin-top: 10px;
+`;
+export const Item = styled.TouchableOpacity`
+  background: #faf7f7;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border: 1px solid #eee;
+  height: 40px;
+`;
+export const Title = styled.Text`
+  font-size: 16px;
+  margin-left: 10px;
+`;
+export const Label = styled.Text`
+  color: grey;
+  font-size: 16px;
 `;
